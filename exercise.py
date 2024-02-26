79dd55c2cdfb55f654cd49a1871e73d78be5fd59
SHERAB WANGCHUK <02230117.csr@rub.edu.bt>
2024-02-26 15:25:44 +0530

    exercise.py

---
+++ exercise.py
@@ -0,0 +1,12 @@
+#Exercise
+input("Enter your age(A): ")
+input("Are you student(S)?(yes / no ):")
+
+if (A <= 12 and S == yes :)
+print("eligible for discount")
+
+elif (A >= 13 or A = 18 and S == yes :)
+print("eligible for discount")
+
+else: 
+    print("not eligible for discount")
