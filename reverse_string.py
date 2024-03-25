26c5e11baf71a30f730155c34c61c5c37ae14bb2
SHERAB WANGCHUK <02230117.csr@rub.edu.bt>
2024-03-25 12:50:16 +0530

    reverse_string.py

---
+++ reverse_string.py
@@ -0,0 +1,12 @@
+def reverse_string(s):
+   if len(s) <= 1:
+       return s
+   
+   return reverse_string(s[1:])+s[0]
+
+print(reverse_string("hello"))
+print(reverse_string("python"))
+
+   
+    
+    
