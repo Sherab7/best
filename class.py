44e71c21f1c9275dfb8a2dda21e173457168b9ac
SHERAB WANGCHUK <02230117.csr@rub.edu.bt>
2024-05-26 15:07:14 +0530

    classacticity.py

---
+++ class.py
@@ -0,0 +1,13 @@
+class dogs:
+    def _init_(self, name, color, age):
+        self.name = name
+        self.color = color
+        self.age = age
+
+dog1 = dogs("Dawa","brown",8)
+dog2 = dogs("Nima","Red",9)
+dog3 = dogs("Blacky","Black",10)
+
+print(dog1.name.color.age)
+print(dog2.name.color.age)
+print(dog3.name.color.age)
